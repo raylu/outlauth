a00d4e6148a96c22cc10e10e570a5dbdeb41e0b5
raylu <lurayl@gmail.com>
2014-09-28 03:04:12 -0700

    refactor message parsing/formatting, handle NICK

--- ircd.py
+++ ircd.py
@@ -24,85 +24,152 @@ class Commands:
 	QUIT = 'QUIT'
 	PRIVMSG = 'PRIVMSG'
 
-class ReplyCodes:
+class RPL:
 	WELCOME = 1
 	YOURHOST = 2
 	CREATED = 3
 	MYINFO = 4
+	WHOISUSER = 311
+	ENDOFWHOIS = 318
+	NAMREPLY = 353
+	ENDOFNAMES = 366
 	MOTD_CONTENT = 372
 	MOTD_START = 375
 	MOTD_END = 376
 
+
+class ClientMessage:
+	''' command, target, text '''
+	def __init__(self, line):
+		self.line = line
+
+		split = line.split(' ', 2) # this is technically wrong, but works for most things
+		self.command = split[0]
+		self.target = split[1]
+		self.text = None
+		if len(split) > 2 and split[2]:
+			self.text = split[2]
+			if self.text[0] == ':':
+				self.text = self.text[1:]
+
+	def __str__(self):
+		return '<ClientMessage>%s' % self.__dict__
+
 class User:
-	def __init__(self, conn, addr, username=None):
+	def __init__(self, conn, addr):
 		self.conn = conn
 		self.addr = addr
-		self.username = username
+		self.nick = None
 		self.channels = []
 
-	@classmethod
-	def parse_username(self, message):
-		username = message.split(' ')[1]
-		return username
+	def handle_conn(self):
+		print('connected by', self.addr)
+		keep_going = True
+		while keep_going:
+			try:
+				data = self.conn.recv(1024)
+			except OSError as e:
+				if e.errno in [errno.EBADF, errno.ECONNRESET]:
+					self.disconnect()
+					break
+				raise
+			if not data:
+				break
+			lines = data.splitlines()
+			for line in lines:
+				line = line.decode('utf-8')
+				if DEBUG:
+					print('<-', line)
+				message = ClientMessage(line)
+				keep_going = self.handle_message(message)
+		print('disconnecting', self.addr)
+		conn.close()
 
-	def _format_message(self, code, message):
-		return ':outlauth {} {} :{}'.format(code, self.username, message)
+	def handle_message(self, msg):
+		cmd = msg.command
+		if cmd == Commands.NICK:
+			old_nick = self.nick
+			if old_nick is not None:
+				del users[self.nick]
+			self.nick = msg.target
+			users[self.nick] = self
+			if old_nick is not None:
+				self.send('NICK', source=old_nick)
+		elif cmd == Commands.USER:
+			self.send(RPL.WELCOME, 'Welcome to outlauth')
+			self.send(RPL.YOURHOST, 'Your host is outlauth, running version 0.0')
+			self.send(RPL.CREATED, 'The server was created today')
+			self.send(RPL.MYINFO, 'outlauth 0.0')
+			self.send(RPL.MOTD_START, '*** Message of the day:')
+			self.send(RPL.MOTD_CONTENT, 'This is the message of the day, bitch')
+			self.send(RPL.MOTD_END, '*** End of message of the day')
+		elif cmd == Commands.QUIT:
+			self.disconnect()
+			return False
+		elif cmd == Commands.MODE:
+			self.mode()
+		elif cmd == Commands.WHOIS:
+			self.whois()
+		elif cmd == Commands.JOIN:
+			self.join(msg)
+		elif cmd == Commands.PRIVMSG:
+			self.privmsg(msg)
+		return True
 
-	def send_message(self, message):
+	def send(self, command, *args, target=None, source=None):
+		if target is None:
+			target = self.nick
+		if args and ' ' in args[-1]:
+			args = list(args)
+			args[-1] = ':' + args[-1]
+		line = '%s %s %s' % (command, target, ' '.join(args))
+		if source is not None:
+			line = ':%s %s' % (source, line)
 		if DEBUG:
-			print('->', message)
-		message = message + '\r\n'
+			print('->', line)
+		line += '\r\n'
 		try:
-			self.conn.sendall(message.encode('utf-8'))
+			self.conn.sendall(line.encode('utf-8'))
 		except OSError as e:
 			if e.errno in [errno.EBADF, errno.ECONNRESET]:
 				self.disconnect()
 			else:
 				raise
 
-	def privmsg(self, message):
-		if not self.username:
+	def mode(self):
+		if not self.nick:
 			return
-		_, channel_name, message = message.split(' ', 2)
-		channel = channels[channel_name]
-		channel.message(self, message)
-
-	def send_notice(self, message):
-		message = self._format_message('NOTICE', message)
-		self.send_message(message)
-
-	def send_reply(self, code, message):
-		message = self._format_message(code, message)
-		self.send_message(message)
-
-	def set_mode(self):
-		self.send_message('{0} MODE {0} :+i'.format(self.username))
 
 	def whois(self):
-		self.send_message(':outlauth 311 {0} {0} outlauth * :dude'.format(self.username))
-		self.send_message(':outlauth 318 {0} :End of WHOIS list'.format(self.username))
+		self.send(RPL.WHOISUSER, self.nick, 'outlauth * :dude')
+		self.send(RPL.ENDOFWHOIS, ':End of WHOIS list')
 
-	def join_channel(self, message):
-		message = message.split(' ')
-		if len(message) > 1:
-			channel_name = message[1]
-			if channel_name not in channels:
-				channels[channel_name] = channel = Channel(channel_name)
-			else:
-				channel = channels[channel_name]
-				if channel in self.channels:
-					return
-			channel.add_user(self)
-			self.channels.append(channel)
-			names = ' '.join((str(user.username) for user in channel.users))
-			self.send_message(':outlauth 353 {0} @ {0} :names'.format(channel.name, names))
-			self.send_message(':outlauth 366 {0} {0} :End of /NAMES list')
+	def join(self, msg):
+		if not self.nick or not msg.target:
+			return
+		if msg.target not in channels:
+			channels[msg.target] = channel = Channel(msg.target)
+		else:
+			channel = channels[msg.target]
+			if channel in self.channels:
+				return
+		channel.add_user(self)
+		self.channels.append(channel)
+		names = ' '.join((str(user.nick) for user in channel.users))
+		self.send(RPL.NAMREPLY, '@', channel.name, names)
+		self.send(RPL.ENDOFNAMES, channel.name, ':End of /NAMES list')
+
+	def privmsg(self, msg):
+		if not self.nick:
+			return
+		channel = channels[msg.target]
+		channel.message(self, msg.text)
 
 	def disconnect(self):
 		for channel in self.channels:
 			channel.remove_user(self)
 		self.conn.close()
-		del users[self.username]
+		del users[self.nick]
 
 class Channel:
 	def __init__(self, name):
@@ -115,68 +182,11 @@ class Channel:
 	def remove_user(self, user):
 		self.users.remove(user)
 
-	def message(self, user, message):
-		line = ':{} PRIVMSG {} :{}'.format(user.username, self.name, message)
+	def message(self, user, text):
 		for u in self.users:
 			if user is u:
 				continue
-			u.send_message(line)
-
-def get_command(message):
-	return message.split(' ')[0]
-
-def run_command(user, cmd, message):
-	if cmd == Commands.NICK:
-		username = User.parse_username(message)
-		user.username = username
-		users[username] = user
-		user.send_notice('*** Nick set to {}'.format(user.username))
-	elif cmd == Commands.USER:
-		user.send_notice('*** Connected')
-		user.send_reply(ReplyCodes.WELCOME, 'Welcome to outlauth')
-		user.send_reply(ReplyCodes.YOURHOST, 'Your host is outlauth, running version 0.0')
-		user.send_reply(ReplyCodes.CREATED, 'The server was created today')
-		user.send_reply(ReplyCodes.MYINFO, 'outlauth 0.0')
-		user.send_reply(ReplyCodes.MOTD_START, '*** Message of the Day:')
-		user.send_reply(ReplyCodes.MOTD_CONTENT, 'This is the message of the day, bitch')
-		user.send_reply(ReplyCodes.MOTD_END, '*** End of Message of the Day')
-	elif cmd == Commands.QUIT:
-		user.disconnect()
-		return False
-	elif user.username is not None:
-		if cmd == Commands.MODE:
-			user.set_mode()
-		elif cmd == Commands.WHOIS:
-			user.whois()
-		elif cmd == Commands.JOIN:
-			user.join_channel(message)
-		elif cmd == Commands.PRIVMSG:
-			user.privmsg(message)
-	return True
-
-def handle_conn(conn, addr):
-	print('connected by', addr)
-	user = User(conn, addr)
-	keep_going = True
-	while keep_going:
-		try:
-			data = conn.recv(1024)
-		except OSError as e:
-			if e.errno in [errno.EBADF, errno.ECONNRESET]:
-				user.disconnect()
-				break
-			raise
-		if not data:
-			break
-		messages = data.splitlines()
-		for message in messages:
-			message = message.decode('utf-8')
-			if DEBUG:
-				print('<-', message)
-			cmd = get_command(message)
-			keep_going = run_command(user, cmd, message)
-	print('disconnecting', addr)
-	conn.close()
+			u.send('PRIVMSG', text, target=self.name, source=user.nick)
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
@@ -185,7 +195,8 @@ s.listen(4)
 try:
 	while True:
 		conn, addr = s.accept()
-		gevent.spawn(handle_conn, conn, addr)
+		user = User(conn, addr)
+		gevent.spawn(user.handle_conn)
 except KeyboardInterrupt:
 	print('closing all connections')
 	for user in list(users.values()):
